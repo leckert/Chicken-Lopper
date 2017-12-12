9bee86b75712f26580131ef21b5db8de88f11a32
Lucien Eckert <leckert@stuy.edu>
2017-12-12 12:12:29 -0500

    gates shocked

--- Chicken.java
+++ Chicken.java
@@ -5,22 +5,36 @@ public class Chicken {
 
       private  InputStreamReader isr;
       private  BufferedReader in;
+      private String chickenStatus;
+      private int resignation;
 
     public Chicken(){
 	isr = new InputStreamReader( System.in );
     in = new BufferedReader( isr );
+    resignation = 0;
+    chickenStatus = "unresigned";
     newGame();
     }
     
     public  void newGame() {
 	System.out.println("Chicken appeared.");
 	System.out.println("~~~~~~~~~~~~~~~~~");
+	while(chickenStatus != "resigned") {
 	System.out.println("what you do boi");
 	String action = "";
 	try {
 	    action = in.readLine();
             } catch ( IOException e ) { }
 	System.out.println("Aight you " + action + ".");
+		resignation += Math.random() * 20;
+	System.out.println("Chicken is now " + resignation + "% resigned \n");
+	if(resignation >= 100) chickenStatus = "resigned";
+        else {
+		System.out.println("shit, chicken bouta " + action + " you!");
+		resignation -= Math.random() * 20;
+	System.out.println("Chicken is now " + resignation + "% resigned \n");
+	}	
+	}
 	System.out.println("chicken resigned");
     }
 
