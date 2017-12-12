cd911f0d93899a689a8a5a00ec4895e8f27d43e7
Lucien Eckert <leckert@stuy.edu>
2017-12-12 12:04:13 -0500

    chicken     resign

--- Chicken.java
+++ Chicken.java
@@ -1,7 +1,31 @@
+import java.io.*;   
+import java.util.*;
+
 public class Chicken {
 
+      private  InputStreamReader isr;
+      private  BufferedReader in;
+
+    public Chicken(){
+	isr = new InputStreamReader( System.in );
+    in = new BufferedReader( isr );
+    newGame();
+    }
+    
+    public  void newGame() {
+	System.out.println("Chicken appeared.");
+	System.out.println("~~~~~~~~~~~~~~~~~");
+	System.out.println("what you do boi");
+	String action = "";
+	try {
+	    action = in.readLine();
+            } catch ( IOException e ) { }
+	System.out.println("Aight you " + action + ".");
+	System.out.println("chicken resigned");
+    }
+
     public static void main(String[] args) {
-	System.out.println("Chicken");
+	Chicken dank = new Chicken();
     }
 
 }
